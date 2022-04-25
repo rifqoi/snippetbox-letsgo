0245629490fec12654edb7544140dee63a95652b
rifqoi <alfurqon.rifqi@gmail.com>
2022-04-26 03:35:31 +0700

    Add route to serving files with mux.Handle

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,14 @@ func main() {
 	mux.HandleFunc("/snippet/create", createSnippet)
 	mux.HandleFunc("/snippet/edit", editSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// StripPrefix buat nge strip /static
+	// e.g /static/image.jpg bakal distrip jadi -> /image.jpg
+	// dari situ ntar server bakal ngirim file dari ".ui/static/image.jpg"
+	// https://stackoverflow.com/questions/27945310/why-do-i-need-to-use-http-stripprefix-to-access-my-static-files
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on http://localhost:3000")
 	err := http.ListenAndServe(":3000", mux)
 	log.Fatal(err)
